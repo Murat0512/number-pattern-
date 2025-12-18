b91dd89e9c4aecf1aa1bc881617784c261f468e1
Murat Karabiyik <167768799+Murat0512@users.noreply.github.com>
2025-12-18 14:00:01 +0000

    Delete email.format.py

--- email.format.py
+++
@@ -1,7 +0,0 @@
-name = input("Enter your first name: ")
-middile_name = input("Enter your middle name: ")
-surname= input("Enter your surname: ")
-email = input("Enter your email address: ")
-
-
-print(name + " " + middile_name + " " + surname + "\nEmail: " + email)
