37a340c95c8f1782566ed2bfe1fedb9867cdce08
Murat Karabiyik <muratkarabiyik@hotmail.co.uk>
2025-12-18 13:54:09 +0000

    Initial commit

---
+++ email.format.py
@@ -0,0 +1,7 @@
+name = input("Enter your first name: ")
+middile_name = input("Enter your middle name: ")
+surname= input("Enter your surname: ")
+email = input("Enter your email address: ")
+
+
+print(name + " " + middile_name + " " + surname + "\nEmail: " + email)
